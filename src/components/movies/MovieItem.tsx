b4c0aabcb70f14d871d46c2b48eb20e132d26900
nahrowi.one <nahrowi.one@gmail.com>
2024-06-30 02:29:05 +0700

    update bug

--- src/components/movies/MovieItem.tsx
+++ src/components/movies/MovieItem.tsx
@@ -1,81 +1,80 @@
 import {
-    ImageBackground,
-    View,
-    Text,
-    StyleSheet,
-    TouchableOpacity,
-  } from "react-native";
-  import { MovieItemProps } from "../../types/app";
-  import { FontAwesome } from "@expo/vector-icons";
-  import { LinearGradient } from "expo-linear-gradient";
-  import { StackActions, useNavigation } from "@react-navigation/native";
-  
-  function MovieItem({ movie, size, coverType }: MovieItemProps): JSX.Element {
-    const navigation = useNavigation();
-    const pushAction = StackActions.push("movieDetail", { id: movie.id });
-  
-    const roundedVoteAverage = movie.vote_average.toFixed(1);
-  
-    return (
-      <TouchableOpacity
-        onPress={() => {
-          navigation.dispatch(pushAction);
+  ImageBackground,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { MovieItemProps } from "../../types/app";
+import { FontAwesome } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { StackActions, useNavigation } from "@react-navigation/native";
+
+function MovieItem({ movie, size, coverType }: MovieItemProps): JSX.Element {
+  const navigation = useNavigation();
+  const pushAction = StackActions.push("movieDetail", { id: movie.id });
+
+  const roundedVoteAverage = movie.vote_average.toFixed(1);
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.dispatch(pushAction);
+      }}
+    >
+      <ImageBackground
+        resizeMode="cover"
+        style={[size, styles.backgroundImage]}
+        imageStyle={styles.backgroundImageStyle}
+        source={{
+          uri: `https://image.tmdb.org/t/p/w500${
+            coverType === "backdrop" ? movie.backdrop_path : movie.poster_path
+          }`,
         }}
       >
-        <ImageBackground
-          resizeMode="cover"
-          style={[size, styles.backgroundImage]}
-          imageStyle={styles.backgroundImageStyle}
-          source={{
-            uri: `https://image.tmdb.org/t/p/w500${
-              coverType === "backdrop" ? movie.backdrop_path : movie.poster_path
-            }`,
-          }}
+        <LinearGradient
+          colors={["#00000000", "rgba(0, 0, 0, 0.7)"]}
+          locations={[0.6, 0.8]}
+          style={styles.gradientStyle}
         >
-          <LinearGradient
-            colors={["#00000000", "rgba(0, 0, 0, 0.7)"]}
-            locations={[0.6, 0.8]}
-            style={styles.gradientStyle}
-          >
-            <Text style={styles.movieTitle}>{movie.title}</Text>
-            <View style={styles.ratingContainer}>
-              <FontAwesome name="star" size={16} color="yellow" />
-              <Text style={styles.rating}>{roundedVoteAverage}</Text>
-            </View>
-          </LinearGradient>
-        </ImageBackground>
-      </TouchableOpacity>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    backgroundImage: {
-      marginRight: 4,
-    },
-    backgroundImageStyle: {
-      borderRadius: 8,
-    },
-    movieTitle: {
-      color: "white",
-    },
-    gradientStyle: {
-      padding: 8,
-      height: "100%",
-      width: "100%",
-      borderRadius: 8,
-      display: "flex",
-      justifyContent: "flex-end",
-    },
-    ratingContainer: {
-      flexDirection: "row",
-      alignItems: "center",
-      gap: 2,
-    },
-    rating: {
-      color: "yellow",
-      fontWeight: "700",
-    },
-  });
-  
-  export default MovieItem;
-  
+          <Text style={styles.movieTitle}>{movie.title}</Text>
+          <View style={styles.ratingContainer}>
+            <FontAwesome name="star" size={16} color="yellow" />
+            <Text style={styles.rating}>{roundedVoteAverage}</Text>
+          </View>
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    marginRight: 4,
+  },
+  backgroundImageStyle: {
+    borderRadius: 8,
+  },
+  movieTitle: {
+    color: "white",
+  },
+  gradientStyle: {
+    padding: 8,
+    height: "100%",
+    width: "100%",
+    borderRadius: 8,
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  ratingContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 2,
+  },
+  rating: {
+    color: "yellow",
+    fontWeight: "700",
+  },
+});
+
+export default MovieItem;
